8a2dc147ff66fc5bd1eb21ab4403c2b8a118f162
Osster <_oster_@mail.ru>
2018-06-25 15:59:21 +0300

    Initial commit

---
+++ app/js/components/top-slider.js
@@ -0,0 +1,29 @@
+var topSlider = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+
+    loop: true,
+
+    roundLengths: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+topSlider.on('slideChange', function (e) {
+    var slider = this;
+    var style = $(slider.slides[slider.activeIndex]).data('style');
+    if (style == 'light') {
+        $('header').removeClass('light').addClass('dark');
+    } else {
+        $('header').removeClass('dark').addClass('light');
+    }
+    //console.log('slideChange style', style);
+});
---
+++ app/js/main.js
@@ -0,0 +1,24 @@
+// Импортируем jQuery
+//= scripts/jquery-3.3.1.min.js
+
+// Импортируем Popper
+//= scripts/popper.js
+
+// Импортируем Bootstrap
+//= scripts/bootstrap.js
+
+
+//= ../../node_modules/jquery-mask-plugin/dist/jquery.mask.js
+//= ../../node_modules/jquery-validation/dist/jquery.validate.js
+//= ../../node_modules/jquery-datetimepicker/build/jquery.datetimepicker.full.min.js
+//= ../../node_modules/swiper/dist/js/swiper.js
+
+//= components/top-slider.js
+
+$(document).ready(function () {
+
+});
+
+$(window).load(function () {
+
+});
