e8da649d67b002c14c2ab1a33fa70f870fd47312
Gpasha <g1ebikpasha@gmail.com>
2018-06-26 17:21:44 +0300

    sliders

--- app/js/components/top-slider.js
+++ app/js/components/top-slider.js
@@ -1,4 +1,4 @@
-var topSlider = new Swiper ('.swiper-container', {
+var topSlider = new Swiper ('.top-slider .swiper-container', {
     // Optional parameters
     direction: 'horizontal',
 
@@ -8,13 +8,13 @@ var topSlider = new Swiper ('.swiper-container', {
 
     // If we need pagination
     pagination: {
-        el: '.swiper-pagination',
+        el: '.top-slider .swiper-pagination',
     },
 
     // Navigation arrows
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.top-slider .swiper-button-next',
+        prevEl: '.top-slider .swiper-button-prev',
     },
 });
 topSlider.on('slideChange', function (e) {
@@ -28,22 +28,44 @@ topSlider.on('slideChange', function (e) {
     //console.log('slideChange style', style);
 });
 
+
 var brandSlider = new Swiper ('.wr-brands .swiper-container', {
     // Optional parameters
+
+    slidesPerView: 8,
+
+    paceBetween: 20,
+
     direction: 'horizontal',
 
     loop: true,
 
-    roundLengths: true,
-
-    // // If we need pagination
-    // pagination: {
-    //     el: '.swiper-pagination'
-    // },
+    //roundLengths: true,
 
     // Navigation arrows
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
+        nextEl: '.wr-brands .swiper-button-next',
+        prevEl: '.wr-brands .swiper-button-prev'
     }
 });
+
+
+// var eventsSlider = new Swiper ('.wr-events .swiper-container', {
+//     // Optional parameters
+//     direction: 'horizontal',
+//
+//     loop: true,
+//
+//     roundLengths: true,
+//
+//     // // If we need pagination
+//     // pagination: {
+//     //     el: '.swiper-pagination'
+//     // },
+//
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.wr-events .swiper-button-next',
+//         prevEl: '.wr-events .swiper-button-prev'
+//     }
+// });
