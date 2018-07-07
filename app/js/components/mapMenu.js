7488cec6b25928f82ced942b5c4ea7b2abdaf735
Dada <dada0net@gmail.com>
2018-07-07 14:06:16 +0300

    7-04 map

--- app/js/components/mapMenu.js
+++ app/js/components/mapMenu.js
@@ -1,18 +1,24 @@
 (function () {
-    $(document).ready(function () {
-        var $body = $('body');
-        var $mapmenuTree = $body.find('.menu-item');
-        var $mapmenuTreeItems = $mapmenuTree.find('.menu-item h3');
+    var $lg = $('.map_menu');
+    var $lg_items = null;
+    var $lg_item_labels = null;
 
-        $mapmenuTreeItems.off('click').on('click', function (e) {
-            var isLink = (e.target.tagName === 'A');
-
-            if (!isLink) {
-                $(this).toggleClass('closed');
+    if ($lg.length > 0) {
+        $lg_items = $lg.find('.map-item');
+        $lg_item_labels = $lg_items.find('.map-item_label');
+        $lg_item_labels.on('click', function () {
+            console.log('click');
+            var wndSize = $(window).width();
+            $(this).parents('.map_menu').find('.active').removeClass('active');
+            var currentItem = $(this).parent('.map-item');
+            currentItem.addClass('active');
+            if (wndSize <= 576) {
+                setTimeout(function () {
+                    $('html, body').animate({
+                        scrollTop: parseInt(currentItem.offset().top) - 80
+                    }, 300);
+                }, 300);
             }
-
-            return isLink;
         });
-
-    });
+    }
 })();
