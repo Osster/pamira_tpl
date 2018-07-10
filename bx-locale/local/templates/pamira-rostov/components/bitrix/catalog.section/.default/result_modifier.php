a4c685bbbdf10211ece99f7d1c62a807ceb37d60
Dada <dada0net@gmail.com>
2018-07-10 20:13:29 +0300

    9.04 kat

---
+++ bx-locale/local/templates/pamira-rostov/components/bitrix/catalog.section/.default/result_modifier.php
@@ -0,0 +1,15 @@
+<? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+
+/**
+ * @var CBitrixComponentTemplate $this
+ * @var CatalogSectionComponent $component
+ */
+
+global $APPLICATION;
+
+$component = $this->getComponent();
+$arParams = $component->applyTemplateModifications();
+
+if (isset($_REQUEST["TYPE"]) && ($_REQUEST["TYPE"] == 'card' || $_REQUEST["TYPE"] == 'line')) {
+    $APPLICATION->set_cookie("SECTION_VIEW_TYPE", strVal($_REQUEST["TYPE"]));
+}
